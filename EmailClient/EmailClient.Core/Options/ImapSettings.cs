6dd50b470bd802f01e99c52a2cd7ad728bcbae7e
cholakovge <cholakovge@gmail.com>
2025-04-04 16:32:34 +0200

    Enchanced settings

--- EmailClient/EmailClient.Core/Options/ImapSettings.cs
+++ EmailClient/EmailClient.Core/Options/ImapSettings.cs
@@ -3,5 +3,13 @@
     public class ImapSettings
     {
         public const string SectionKey = nameof(ImapSettings);
+
+        public string Host { get; set; } = string.Empty;
+
+        public string Port { get; set; } = string.Empty;
+
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
     }
 }
